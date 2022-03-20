2c63983e2d00679ac326910f80511487c7fce091
Digital Mind <jremesz@gmail.com>
2022-03-20 16:54:32 +0100

    navbar body an dfooter clean components with icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
-      <div className="bg-slate-800 text-slate-100">
-        <h1>Hello</h1>
+      <div className="bg-slate-100 text-slate-900">
+        <Router>
+          <Navbar />
+        </Router>
+        <Body />
+        <Footer />
       </div>
     </>
   );
